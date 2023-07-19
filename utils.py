977c34d2898f6cb658d7b67bd8852e76177ea787
vmcvastry <009valerio@gmail.com>
2023-07-19 16:57:38 +0200

    Show rescaled img

--- utils.py
+++ utils.py
@@ -10,7 +10,21 @@ from rasterio.plot import show
 
 
 def print_dataset_image(img):
-    show(img.read([3, 2, 1]))
+    # show(img.read([3, 2, 1]))
+    img = np.array(img.read([3, 2, 1, 4]))
+    min_values = np.array([0, 0, 0, 0])
+    max_values = np.array([0, 0, 0, 0])
+    for i in range(4):
+        min_values[i] = img[i, :, :].min()
+        max_values[i] = img[i, :, :].max()
+    normalized_img = np.zeros(img.shape, dtype=np.float32)
+    for i in range(4):
+        normalized_img[i, :, :] = (img[i, :, :] - min_values[i]) / (
+            max_values[i] - min_values[i]
+        )
+    i = normalized_img[:3, :, :]
+    i *= 255
+    show(i.astype(np.uint8))
 
 
 def print_mask(mask):
