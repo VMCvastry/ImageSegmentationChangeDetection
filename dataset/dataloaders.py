eba8d36cc39f5da0ce8a2ea1f05d337e487269f6
vmcvastry <009valerio@gmail.com>
2023-08-10 08:11:03 +0200

    Deterministic loader

--- dataset/dataloaders.py
+++ dataset/dataloaders.py
@@ -2,6 +2,8 @@ import torch
 import torch.utils.data
 from dataset import DynamicEarthNet
 
+SEED = 42
+
 
 def get_dataloaders(
     root, batch_size=4, binary_change_detection=True, subset_percentage=1
@@ -13,22 +15,33 @@ def get_dataloaders(
     train_size = int(0.8 * len(data))
     test_size = len(data) - train_size
     train_dataset, test_dataset = torch.utils.data.random_split(
-        data, [train_size, test_size]
+        data, [train_size, test_size], generator=torch.Generator().manual_seed(SEED)
     )
     val_size = int(0.5 * test_size)
     test_size = test_size - val_size
     test_dataset, val_dataset = torch.utils.data.random_split(
-        test_dataset, [test_size, val_size]
+        test_dataset,
+        [test_size, val_size],
+        generator=torch.Generator().manual_seed(SEED),
     )
     print(len(train_dataset), len(test_dataset), len(val_dataset))
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        generator=torch.Generator().manual_seed(SEED),
     )
     test_loader = torch.utils.data.DataLoader(
-        test_dataset, batch_size=batch_size, shuffle=True
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        generator=torch.Generator().manual_seed(SEED),
     )
     accuracy_loader = torch.utils.data.DataLoader(data, batch_size=512, shuffle=True)
     val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=batch_size, shuffle=True
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        generator=torch.Generator().manual_seed(SEED),
     )
     return train_loader, test_loader, val_loader, accuracy_loader
