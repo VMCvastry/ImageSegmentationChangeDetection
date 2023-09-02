c6497034fa818fb2a63f5d2db3f5035158b0d5d0
vmcvastry <009valerio@gmail.com>
2023-09-02 22:46:02 +0200

    Online labels

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,7 +2,8 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
-from constants import mean_dataset, std_dataset
+from constants import mean_dataset, std_dataset, ONLINE_LABELS
+from dataset.creation_utils import build_label
 from .dataset_utils import (
     get_pairs,
     load_img,
@@ -14,20 +15,25 @@ from .dataset_utils import (
 from utils import print_mask
 
 
+def get_label(files, root, binary_change_detection):
+    if ONLINE_LABELS:
+        return build_label(files, binary_change_detection)
+    else:
+        label_file = get_computed_labels_files(files, root, binary_change_detection)[0]
+        return np.load(label_file)
+
+
 class DynamicEarthNet(Dataset):
     def __init__(self, root, binary_change_detection=True):
+        self.root = root
         self.binary_change_detection = binary_change_detection
         self.normalize = transforms.Normalize(
             mean=np.concatenate((mean_dataset, mean_dataset)),
             std=np.concatenate((std_dataset, std_dataset)),
         )  # 8channels =(4+4)
-
         images_sources, labels_sources = detect_data(root)
-        self.img_pairs, label_pairs = get_pairs(images_sources, labels_sources)
+        self.img_pairs, self.label_pairs = get_pairs(images_sources, labels_sources)
         print(f"Found {len(self.img_pairs)} pairs of images and labels")
-        self.labels = get_computed_labels_files(
-            label_pairs, root, self.binary_change_detection
-        )
         # self.img_pairs, self.labels = balance_dataset(
         #     self.img_pairs, self.labels, self.binary_change_detection
         # )
@@ -42,10 +48,10 @@ class DynamicEarthNet(Dataset):
         img = np.transpose(img, (2, 0, 1))  # 8x1024x1024
         img = torch.from_numpy(np.array(img, dtype=np.float32))
         img = self.normalize(img)
-        label_file = self.labels[index]
-        label = np.load(label_file)
-        if not self.binary_change_detection:
-            label = get_one_hot_from_mask(label)
+        label_files = self.label_pairs[index]
+        label = get_label(label_files, self.root, self.binary_change_detection)
+        # if not self.binary_change_detection:
+        #     label = get_one_hot_from_mask(label)
         label = torch.from_numpy(np.array(label, dtype=np.float32))  # why float?
         return img, label
 
